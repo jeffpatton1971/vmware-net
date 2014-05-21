7d1059ea54fe69910b7e97c2335045f6417fe022
Jeff Patton <jspatton@ku.edu>
2014-05-21 17:29:30 -0500

    Moved usings into namespace, removed unused usings and added proper comment blocks.

--- vmware-net/functions.cs
+++ vmware-net/functions.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Web;
-using VMware.Vim;
-
-namespace vmware_net
+﻿namespace vmware_net
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using VMware.Vim;
+
     public class functions
     {
+        /// <summary>
+        /// This function returns a string that represents a proper vmware sdk url
+        /// https://server.domain/sdk
+        /// </summary>
+        /// <param name="viServer"></param>
+        /// <returns>A string containing a valid URI</returns>
         public static string ValidateServer(string viServer)
         {
             //
@@ -54,6 +58,18 @@ namespace vmware_net
             }
             return viServer;
         }
+        /// <summary>
+        /// This function is a wrapper for the FindEntityViews method of the vimclient. It
+        /// allows you to easily return a list of objects based on a filter. The filter
+        /// is a NameValueCollection object that contains a property name (string) and a
+        /// property value (string).
+        /// </summary>
+        /// <typeparam name="T">This is the type of object we want to get</typeparam>
+        /// <param name="vimClient">This is our client connection</param>
+        /// <param name="beginEntity">This is a ManagedObjectReference to start at</param>
+        /// <param name="filter">A NameValueCollection object that contains our filter</param>
+        /// <param name="properties">An array of properties to return, all are returned if null</param>
+        /// <returns>Returns the appropriate objects that you asked for.</returns>
         public static List<T> GetEntities<T>(VimClient vimClient, ManagedObjectReference beginEntity, NameValueCollection filter, string[] properties)
         {
             List<T> things = new List<T>();
@@ -66,18 +82,47 @@ namespace vmware_net
             }
             return things;
         }
+        /// <summary>
+        /// This function is a wrapper for the FindEntityView method of the vimclient. It
+        /// allows you to easily return an object based on a filter. The filter is a 
+        /// NameValueCollection object that contains a property name (string) and a property value (string).
+        /// </summary>
+        /// <typeparam name="T">This is the type of object we want to get</typeparam>
+        /// <param name="vimClient">This is our client connection</param>
+        /// <param name="beginEntity">This is a ManagedObjectReference to start at</param>
+        /// <param name="filter">A NameValueCollection object that contains our filter</param>
+        /// <param name="properties">An array of properties to return, all are returned if null</param>
+        /// <returns>Returns the appropriate objectsthat you asked for.</returns>
         public static T GetEntity<T>(VimClient vimClient, ManagedObjectReference beginEntity, NameValueCollection filter, string[] properties)
         {
             EntityViewBase vBase = vimClient.FindEntityView(typeof(T), beginEntity, filter, properties);
             T thing = (T)(object)vBase;
             return thing;
         }
+        /// <summary>
+        /// This function is a wrapper for the GetView method of the vimclient. It returns 
+        /// and object when passed in a proper ManagedObjectReference. It will return an
+        /// array of properties if asked, otherwise all properties are returned.
+        /// </summary>
+        /// <typeparam name="T">This is the type of object we want to get</typeparam>
+        /// <param name="vimClient">This is our client connection</param>
+        /// <param name="moRef">This is the ManagedObjectReference of the object to return</param>
+        /// <param name="properties">An array of properties to return, all are returned if null</param>
+        /// <returns>Returns the appropriate objectsthat you asked for.</returns>
         public static T GetObject<T>(VimClient vimClient, ManagedObjectReference moRef, string[] properties)
         {
             ViewBase vBase = vimClient.GetView(moRef, properties);
             T thisObject = (T)(object)vBase;
             return thisObject;
         }
+        /// <summary>
+        /// This function returns a valid VimClient connection object. This is used throughout
+        /// to establish a connection to the SDK server in order to query and perform tasks.
+        /// </summary>
+        /// <param name="viServer">A properly validated SDK URI</param>
+        /// <param name="viUser">A user with permissions on the server</param>
+        /// <param name="viPassword">The password of the user with permissions on the server</param>
+        /// <returns>A VimClient object</returns>
         public static VimClient ConnectServer(string viServer, string viUser, string viPassword)
         {
             //
